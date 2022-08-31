63165137f97d9d7321790b080bd8b9e2ccccad60
Christopher Hill <cjameshill@gmail.com>
2022-08-31 15:51:50 +0000

    Added window postmessage testing

--- src/views/VesselIntegrationView.vue
+++ src/views/VesselIntegrationView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import { ref } from "vue";
+const postMessage = () => {
+  const payload = {
+    testing: "hello world"
+  }
+  window.opener.postMessage(payload);
+}
 </script>
 
 <template>
   <main class="w-4/5 flex flex-col justify-center items-center space-y-4">
     <h1 class="text-lg font-medium">Vessel Integration</h1>
+    <button @click="postMessage" class="p-4 bg-gray-100 hover:bg-gray-300 rounded-xl">Message</button>
   </main>
 </template>
