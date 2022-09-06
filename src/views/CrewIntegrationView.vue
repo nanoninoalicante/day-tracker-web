a47341c68fd09fcd28886b8bb56438630bc77f57
Christopher Hill <cjameshill@gmail.com>
2022-09-06 10:00:22 +0000

    Added url

--- src/views/CrewIntegrationView.vue
+++ src/views/CrewIntegrationView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { onMounted, reactive, computed } from 'vue';
-import { useStorage, useLastChanged } from '@vueuse/core'
+import { onMounted, ref, computed } from 'vue';
+import { useStorage, useLastChanged, useEventListener } from '@vueuse/core'
 const inputData = useStorage('cp-crew-button-data', {
   data: {
     "cpPopupUrl": "https://master-dashboard-v1-ojo6h3z4mq-ez.a.run.app/crewlanding",
@@ -21,6 +21,8 @@ const inputData = useStorage('cp-crew-button-data', {
   }
 });
 const lastChanged = useLastChanged(inputData.value)
+
+const fullPoupupUrl = ref("");
 const inputs = computed(() => {
   let arr = [];
   for (const input in inputData.value.data) {
@@ -31,8 +33,13 @@ const inputs = computed(() => {
 const update = () => {
   window.location.reload();
 }
-onMounted(() => {
 
+useEventListener(window, 'message', (message: any) => {
+  console.log('message origin: ', message.origin)
+  console.log('message: ', message.data)
+  if (message.data?.url) {
+    fullPoupupUrl.value = message.data?.url;
+  }
 })
 </script>
 
@@ -40,27 +47,33 @@ onMounted(() => {
   <main class="w-full md:w-4/5 flex flex-col justify-center items-center space-y-4">
 
     <h1 id="title" class="text-lg font-medium">Crew Integration</h1>
-    <div id="cp-holder-1 w-full">
-      <button v-if="lastChanged" @click="update"
-        class="my-4 p-4 rounded-xl bg-yellow-400 hover:bg-gray-400 text-xl font-medium">Changed - Update</button>
-      <div id="cp-agency-crew-profile-button" :data-cp-partner="inputData.data.cpPartner"
-        :data-cp-user-email="inputData.data.cpUserEmail" :data-cp-user-id="inputData.data.cpUserId"
-        :data-cp-first-name="inputData.data.cpFirstName" :data-cp-last-name="inputData.data.cpLastName"
-        :data-cp-nationality="inputData.data.cpNationality" :data-cp-dob="inputData.data.cpDob"
-        :data-cp-gender="inputData.data.cpGender" :data-cp-country-code="inputData.data.cpCountryCode"
-        :data-cp-phone="inputData.data.cpPhone" :data-cp-street-address="inputData.data.cpStreetAddress"
-        :data-cp-city="inputData.data.cpCity" :data-cp-state="inputData.data.cpState"
-        :data-cp-country="inputData.data.cpCountry" :data-cp-popup-url="inputData.data.cpPopupUrl">CP Button Loading
-      </div>
-      <div class="flex flex-col w-full mt-8">
-        <div v-for="item in inputs" class="my-1">
-          <label class="text-sm italic ml-4 font-medium text-gray-400" for="inputData.data[item.key]">{{ item.key
-          }}</label>
-          <input v-model="inputData.data[item.key]" name="inputData.data[item.key]" type="text"
-            class="p-2 border-2 border-gray-200 w-full rounded-xl" />
-        </div>
-      </div>
-    </div>
+    <div id="cp-holder-1" class="flex justify-center flex-col items-center">
+			<button v-if="lastChanged" @click="update"
+				class="my-4 p-4 rounded-xl bg-yellow-400 hover:bg-gray-400 text-xl font-medium">Changed -
+				Update</button>
+			<div id="cp-agency-crew-profile-button" :data-cp-partner="inputData.data.cpPartner"
+				:data-cp-user-email="inputData.data.cpUserEmail" :data-cp-user-id="inputData.data.cpUserId"
+				:data-cp-first-name="inputData.data.cpFirstName" :data-cp-last-name="inputData.data.cpLastName"
+				:data-cp-nationality="inputData.data.cpNationality" :data-cp-dob="inputData.data.cpDob"
+				:data-cp-gender="inputData.data.cpGender" :data-cp-country-code="inputData.data.cpCountryCode"
+				:data-cp-phone="inputData.data.cpPhone" :data-cp-street-address="inputData.data.cpStreetAddress"
+				:data-cp-city="inputData.data.cpCity" :data-cp-state="inputData.data.cpState"
+				:data-cp-country="inputData.data.cpCountry" :data-cp-popup-url="inputData.data.cpPopupUrl">CP Button
+				Loading
+			</div>
+			<div class="flex mt-4">
+				<p>Url: <span class="text-sm text-gray-500">{{fullPoupupUrl}}</span></p>
+			</div>
+			<div class="flex flex-col w-full md:w-[400px] mt-8">
+				<div v-for="item in inputs" class="my-1">
+					<label class="text-sm italic ml-4 font-medium text-gray-400" for="inputData.data[item.key]">{{
+					item.key
+					}}</label>
+					<input v-model="inputData.data[item.key]" name="inputData.data[item.key]" type="text"
+						class="p-2 border-2 border-gray-200 w-full rounded-xl" />
+				</div>
+			</div>
+		</div>
 
   </main>
 </template>
