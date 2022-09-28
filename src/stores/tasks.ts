e31d5599cdbf15cdd4dc8c4e095658495cfb9ba7
Christopher Hill <christopher@nanonino.com>
2022-09-28 12:02:44 +0200

    Addded api base url as env var

--- src/stores/tasks.ts
+++ src/stores/tasks.ts
@@ -2,9 +2,14 @@ import { defineStore } from 'pinia'
 import moment from "moment";
 import { orderBy } from 'lodash';
 import * as uuid from "uuid";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+interface State {
+  tasks: any[]
+}
+
 export const useTasksStore = defineStore({
   id: 'tasks',
-  state: () => ({
+  state: (): State => ({
     tasks: []
   }),
   getters: {
@@ -20,7 +25,7 @@ export const useTasksStore = defineStore({
   },
   actions: {
     async getAll() {
-      const response = await fetch("https://day-tracker-api-dev.nanonino.workers.dev/");
+      const response = await fetch(`${API_BASE_URL}`);
       const jsonResponse = await response.json();
       jsonResponse.result.map((item: any) => {
         this.tasks.push(JSON.parse(item))
@@ -29,11 +34,11 @@ export const useTasksStore = defineStore({
     async add(newTaskData: any) {
       const id = uuid.v4();
       const newTask = { id, name: newTaskData.name, createdAt: new Date().valueOf() }
-      const response = await fetch("https://day-tracker-api-dev.nanonino.workers.dev/", { method: "POST", body: JSON.stringify(newTask) });
+      const response = await fetch(`${API_BASE_URL}`, { method: "POST", body: JSON.stringify(newTask) });
       this.tasks.push(newTask)
     },
     async deleteTask(id: string, score: any) {
-      const response = await fetch(`https://day-tracker-api-dev.nanonino.workers.dev/${score}`, { method: "POST" });
+      const response = await fetch(`${API_BASE_URL}/${score}`, { method: "POST" });
       console.log("score: ", score);
       console.log("response: ", await response.json())
       const index = this.tasks.findIndex((task: any) => task.id === id);
